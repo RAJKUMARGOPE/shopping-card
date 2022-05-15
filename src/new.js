5f0ce574221111a990a1a3b82304c7becf110c43
RAJKUMARGOPE <52957604+RAJKUMARGOPE@users.noreply.github.com>
2022-05-16 02:12:45 +0530

    first commit

---
+++ src/new.js
@@ -0,0 +1,12 @@
+var a = {
+fname: 'rohan',
+lname: 'baba',
+age:   32,
+email: 'rohan@gamil',
+favMovies: ['dhoom','soley','hum'],
+salary : function(){
+    return 34000;
+}
+
+}
+console.log(a.salary());
